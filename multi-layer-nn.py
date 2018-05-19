4c448d7881c89a410a714f7e292c867fdfeee3ad
arjung128 <arjung2@illinois.edu>
2018-05-19 10:44:17 +0900

    basic model implemented

--- multi-layer-nn.py
+++ multi-layer-nn.py
@@ -59,11 +59,6 @@ theta = (w1, b1, w2, b2)
 def squared_loss(y, y_hat):
     return np.dot((y - y_hat),(y - y_hat))
 
-# Output Layer
-def binary_cross_entropy(y, y_hat):
-    return np.sum(-((y * np.log(y_hat)) + ((1-y) * np.log(1 - y_hat))))
-# commented in source to be output layer, but isn't this another loss function?
-
 # Wrapper around the Neural Network
 def neural_network(x, theta):
     w1, b1, w2, b2 = theta
@@ -88,19 +83,16 @@ def update_theta(theta, delta, alpha):
 
 # Compute Gradient
 grad_objective = grad(objective)
-# why are we taking the derivative of the loss? what is this derivative with respect to?
-# how does this get us the w1_delta and so on?
 
 
 # Train the Neural Network
-epochs = 10
-print("RMSE before training:", sklearn.metrics.mean_squared_error(D[1], neural_network(D[0], theta))
-for i in xrange(0, epochs)
-    for j in xrange(0, examples)
-        delta[0] = grad(objective(theta[0], j))
-        delta[1] = grad(objective(theta[1], j))
-        delta[2] = grad(objective(theta[2], j))
-        delta[3] = grad(objective(theta[3], j))
+epochs = 1000
+
+print("RMSE before training:", sklearn.metrics.mean_squared_error(D[1], neural_network(D[0], theta)))
+
+for i in range(epochs):
+    for j in range(examples):
+        delta = grad_objective(theta, j)
         theta = update_theta(theta,delta, 0.01)
 
-print("RMSE after training:", sklearn.metrics.mean_squared_error(D[1], neural_network(D[0], theta))
+print("RMSE after training:", sklearn.metrics.mean_squared_error(D[1], neural_network(D[0], theta)))
